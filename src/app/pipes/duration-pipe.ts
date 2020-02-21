7a13fd701662a1268dc9734ab9300a82635eae45
nachodlv <ignacio.delavega@ing.austral.edu.ar>
2020-02-21 12:34:19 +0100

    Fix bug with project dates

--- src/app/pipes/duration-pipe.ts
+++ src/app/pipes/duration-pipe.ts
@@ -6,7 +6,9 @@ export class DurationPipe implements PipeTransform {
   transform(value: TimeTaken): string {
     let word: string = value.years === 0 ? '' : `${value.years} ${(value.years === 1 ? 'year' : 'years')}`;
     word += value.months === 0 ? '' : `${value.months} ${(value.months === 1 ? 'month' : 'months')}`;
-    word += value.weeks !== undefined && value.weeks === 0 ? '' : `${value.weeks} ${(value.weeks === 1 ? 'week' : 'weeks')}`;
+    if (value.weeks !== undefined) {
+      word += value.weeks === 0 ? '' : `${value.weeks} ${(value.weeks === 1 ? 'week' : 'weeks')}`;
+    }
     return word;
   }
 }
