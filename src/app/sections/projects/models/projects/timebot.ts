c2b4a98be8691b1437707ff9e12a3047b9e6bace
Ignacio de la Vega <ignacio.delavega@ing.austral.edu.ar>
2020-06-11 21:57:28 -0300

    Fix typos in timebot project

--- src/app/sections/projects/models/projects/timebot.ts
+++ src/app/sections/projects/models/projects/timebot.ts
@@ -13,17 +13,17 @@ const projectTimeout = new Project('Timebot',
     new ProjectMedia('assets/images/projects/timebot/timebot5.png', ProjectMediaType.IMAGE),
   ], [
     'Timebot was made for a game jam from gamedev.tv. The game consists in a robot that needs to test a tool to stop time ' +
-    'by completing different tasks such as destroying another robots',
-    'The game was developed by four people including me. Two music composer, one artist and me as a programmer'
+    'by completing different tasks such as destroying another robots.',
+    'The game was developed by four people including me. Two music composer, one artist and me as a programmer.'
   ], {
     startDate: new Date(2020, 4, 1),
     finishDate: new Date(2020, 4, 31),
     timeItTook: {years: 0, months: 1,}
   },
   undefined, [
-    'Load and unload scenes asynchronously in Unity in a fast and efficient way for the different levels',
-    'Learned how use Unity\'s Tilemap for designing leveles',
-    'Learned how to use Unity\'s Trail renderer and 2D lights'
+    'Load and unload scenes asynchronously in Unity in a fast and efficient way for the different levels.',
+    'Learned how use Unity\'s Tilemap for designing levels.',
+    'Learned how to use Unity\'s Trail renderer and 2D lights.'
   ], [
     {name: 'Game', link: 'https://jarambarri.itch.io/timebot'},
     {name: 'Source code', link: 'https://github.com/Nachodlv/timebot'},
